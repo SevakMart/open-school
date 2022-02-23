bd34df1f590face3ef33f4f473bfeb16e582425f
Vahann <yan.vahan@gmail.com>
2022-02-23 22:53:31 +0300

    OS_2 Integrate_Spring_Boot_Web Integrate_Spring_Boot_Test

---
+++ os-server/src/main/java/app/openschool/OpenSchoolApplication.java
@@ -0,0 +1,12 @@
+package app.openschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolApplication.class, args);
+    }
+
+}
---
+++ os-server/src/test/java/app/openschool/OpenSchoolApplicationTest.java
@@ -0,0 +1,12 @@
+package app.openschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OpenSchoolApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
+
