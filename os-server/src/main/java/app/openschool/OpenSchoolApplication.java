efa55b951dd08c322a187e6d90a1591002859756
Sargis Sargsyan <sargiss987@gmail.com>
2022-02-26 00:28:52 +0400

    check googl java format with gradle checkstyle plugin

--- os-server/src/main/java/app/openschool/OpenSchoolApplication.java
+++ os-server/src/main/java/app/openschool/OpenSchoolApplication.java
@@ -3,10 +3,15 @@ package app.openschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Useful Javadoc.
+ */
+
 @SpringBootApplication
 public class OpenSchoolApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OpenSchoolApplication.class, args);
-    }
 
+  public static void main(String[] args) {
+
+    SpringApplication.run(OpenSchoolApplication.class, args);
+  }
 }
--- os-server/src/test/java/app/openschool/OpenSchoolApplicationTest.java
+++ os-server/src/test/java/app/openschool/OpenSchoolApplicationTest.java
@@ -3,10 +3,13 @@ package app.openschool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Useful Javadoc.
+ */
+
 @SpringBootTest
 public class OpenSchoolApplicationTest {
 
-    @Test
-    void contextLoads() {}
+  @Test
+  void contextLoads() {}
 }
-
