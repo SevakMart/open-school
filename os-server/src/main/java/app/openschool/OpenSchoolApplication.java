3c3b6c4ab69d2cf0e40eca251cb5438adbe2b589
Vahann <yan.vahan@gmail.com>
2022-02-25 16:41:56 +0300

    OS_5 Implement a CI pipeline that runs after merge

--- os-server/src/main/java/app/openschool/OpenSchoolApplication.java
+++ os-server/src/main/java/app/openschool/OpenSchoolApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenSchoolApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(OpenSchoolApplication.class, args);
     }
 
