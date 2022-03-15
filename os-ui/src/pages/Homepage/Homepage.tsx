86686c3ac2e33e0eb47dbde8baacd1e8769ff82b
Avedis Demirdjian <avedis_demirdjian@epam.com>
2022-03-15 11:26:05 +0400

    Add start your journey section to hompage

--- os-ui/src/pages/Homepage/Homepage.tsx
+++ os-ui/src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,22 @@
 import Header from '../../component/Header/Header';
 import Footer from '../../component/Footer/Footer';
+import Button from '../../component/Button/Button';
+import styles from './Homepage.module.scss';
 
-const Homepage = () => (
-  <>
-    <Header />
-    <h1>Hello</h1>
-    <h1>World</h1>
-    <Footer />
-  </>
-);
+const Homepage = () => {
+  const { mainContainer, buttonContainer } = styles;
+  return (
+    <>
+      <Header />
+      <div className={mainContainer}>
+        <h2>Start Your Journey Now!</h2>
+        <div className={buttonContainer}>
+          <Button>Sign up as a Student</Button>
+          <Button>sign up as a mentor</Button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Homepage;
