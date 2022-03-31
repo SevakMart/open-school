4e20d600193b7226724f1531efb3dc8437ae4fcb
Avedis Demirdjian <avedis_demirdjian@epam.com>
2022-03-31 13:52:22 +0400

    Remove extra code from os-server

--- os-server/src/main/java/app/openschool/coursemanagement/CourseController.java
+++ os-server/src/main/java/app/openschool/coursemanagement/CourseController.java
@@ -5,14 +5,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin("http://localhost:3000")
 public class CourseController {
 
   private final CourseService courseService;
--- os-server/src/main/java/app/openschool/usermanagement/UserController.java
+++ os-server/src/main/java/app/openschool/usermanagement/UserController.java
@@ -13,12 +13,16 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping ("/api/v1")
-@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/v1")
 public class UserController {
 
   public static final String SUCCESSFULLY_REGISTERED = " you've successfully registered";
@@ -28,8 +32,8 @@ public class UserController {
     this.userService = userService;
   }
 
-  @PostMapping ("/register")
-  @ResponseStatus (CREATED)
+  @PostMapping("/register")
+  @ResponseStatus(CREATED)
   @Operation (summary = "register students")
   public ResponseEntity<UserRegistrationHttpResponse> register(
       @Valid @RequestBody UserRegistrationDto userDto) {
@@ -41,7 +45,7 @@ public class UserController {
     return new ResponseEntity<>(httpResponse, CREATED);
   }
 
-  @GetMapping ("/mentors")
+  @GetMapping("/mentors")
   @Operation(summary = "find all mentors")
   public ResponseEntity<Page<MentorDto>> findAllMentors(Pageable pageable) {
     return ResponseEntity.ok(this.userService.findAllMentors(pageable));
