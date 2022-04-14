5eb303bb94ed347a844d05876d92e5d21281d812
Avedis Demirdjian <avedis_demirdjian@epam.com>
2022-04-14 20:07:23 +0400

    Remove temporary added code form os-server

--- os-server/src/main/java/app/openschool/common/security/SecurityConfigurationDisable.java
+++ os-server/src/main/java/app/openschool/common/security/SecurityConfigurationDisable.java
@@ -1,9 +1,7 @@
 package app.openschool.common.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,11 +14,6 @@ public class SecurityConfigurationDisable extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(WebSecurity web) throws Exception {
     web.ignoring().antMatchers("/**");
-  }
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
   }
 }
 
