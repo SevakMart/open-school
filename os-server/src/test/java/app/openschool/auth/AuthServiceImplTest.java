bd810e2cbde8cda3579b5a749b104b5ed3b52c65
Hakob Diloyan <hakob_diloyan@epam.com>
2022-04-25 21:43:27 +0400

    Have been added unit tests

--- os-server/src/test/java/app/openschool/auth/AuthServiceImplTest.java
+++ os-server/src/test/java/app/openschool/auth/AuthServiceImplTest.java
@@ -1,22 +1,31 @@
 package app.openschool.auth;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import app.openschool.auth.dto.ResetPasswordRequest;
 import app.openschool.auth.dto.UserRegistrationDto;
 import app.openschool.auth.exception.EmailAlreadyExistException;
+import app.openschool.auth.exception.EmailNotExistsException;
 import app.openschool.auth.exception.EmailNotFoundException;
+import app.openschool.auth.exception.NotMatchingPasswordsException;
+import app.openschool.auth.exception.ResetPasswordTokenNotFoundException;
 import app.openschool.common.communication.Communication;
 import app.openschool.user.User;
 import app.openschool.user.UserRepository;
+import java.io.UnsupportedEncodingException;
+import javax.mail.MessagingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -71,5 +80,51 @@ public class AuthServiceImplTest {
 
     assertThatThrownBy(() -> authService.loadUserByUsername("testEmail"))
         .isInstanceOf(EmailNotFoundException.class);
+  }
+
+  @Test
+  void updateResetPasswordTokenUserWithGivenEmailNotFound() {
+    when(userRepository.findUserByEmail("test@gmail.com")).thenReturn(null);
+    assertThatThrownBy(() -> authService.updateResetPasswordToken("test@gmail.com"))
+        .isInstanceOf(EmailNotExistsException.class);
+    verify(userRepository, Mockito.times(1)).findUserByEmail("test@gmail.com");
+  }
+
+  @Test
+  void updateResetPasswordToken() throws MessagingException, UnsupportedEncodingException {
+    User user = new User();
+    when(userRepository.findUserByEmail("test@gmail.com")).thenReturn(user);
+    authService.updateResetPasswordToken("test@gmail.com");
+    verify(userRepository, Mockito.times(1)).findUserByEmail("test@gmail.com");
+    assertNotNull(userRepository.findUserByEmail("test@gmail.com"));
+    assertNotNull(user.getResetPasswordToken());
+    verify(userRepository, Mockito.times(1)).save(user);
+  }
+
+  @Test
+  void restPasswordTokenFails() {
+
+    assertThatThrownBy(
+            () ->
+                authService.resetPassword(
+                    new ResetPasswordRequest("5597", "test@gmail.com", "t@gmail.com")))
+        .isInstanceOf(NotMatchingPasswordsException.class);
+    assertThatThrownBy(
+            () ->
+                authService.resetPassword(
+                    new ResetPasswordRequest("5597", "test@gmail.com", "test@gmail.com")))
+        .isInstanceOf(ResetPasswordTokenNotFoundException.class);
+    verify(userRepository, Mockito.times(1)).findByResetPasswordToken("5597");
+  }
+
+  @Test
+  void restPasswordToken() {
+    User user = new User();
+    user.setResetPasswordToken("7483");
+    when(userRepository.findByResetPasswordToken("7483")).thenReturn(user);
+    authService.resetPassword(new ResetPasswordRequest("7483", "test@gmail.com", "test@gmail.com"));
+    verify(userRepository, Mockito.times(1)).findByResetPasswordToken("7483");
+    assertNotNull(userRepository.findByResetPasswordToken("7483"));
+    verify(userRepository, Mockito.times(1)).save(user);
   }
 }
