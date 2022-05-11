986045e6ed40553c4df074385eb9a5101a9eb54e
Sargis Sargsyan <sargiss987@gmail.com>
2022-05-11 22:13:49 +0400

    OS-60:refactoring

---
+++ os-server/out/production/resources/db/migration/h2/V1_0_5__update.sql
@@ -0,0 +1,33 @@
+ALTER TABLE user
+ADD COLUMN enabled BOOLEAN;
+
+-- -----------------------------------------------------
+-- verification_token
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS reset_password_token (
+ id BIGINT NOT NULL AUTO_INCREMENT,
+ token VARCHAR(30) NOT NULL,
+ user_id BIGINT NOT NULL,
+ created_at TIMESTAMP NOT NULL,
+ PRIMARY KEY (id),
+ CONSTRAINT fk_reset_password_token
+ FOREIGN KEY (user_id)
+ REFERENCES user (id)
+ ON UPDATE CASCADE
+ ON DELETE CASCADE);
+
+
+CREATE TABLE IF NOT EXISTS verification_token (
+ id BIGINT NOT NULL AUTO_INCREMENT,
+ token VARCHAR(45) NOT NULL,
+ user_id BIGINT NOT NULL,
+ created_date TIMESTAMP NOT NULL,
+ PRIMARY KEY (id),
+ CONSTRAINT fk_verification_user
+ FOREIGN KEY (user_id)
+ REFERENCES user (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE);
+
+CREATE INDEX fk_reset_password_token_idx ON reset_password_token (user_id ASC);
+CREATE INDEX fk_verification_user_idx ON verification_token (user_id ASC);
