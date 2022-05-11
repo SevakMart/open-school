5a40b2cc656710793e65e73faddc9b4e5da8cb8d
Sargis Sargsyan <sargiss987@gmail.com>
2022-05-11 21:01:44 +0400

    OS-60:deleted opanapi profile

--- os-server/src/main/java/app/openschool/common/configuration/DevConfig.java
+++ os-server/src/main/java/app/openschool/common/configuration/DevConfig.java
@@ -6,14 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("openapi")
+@Profile("test")
 public class DevConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "*")
+        .allowedOrigins("*")
         .exposedHeaders("Authorization");
   }
 }
--- os-server/src/main/java/app/openschool/common/security/SecurityConfiguration.java
+++ os-server/src/main/java/app/openschool/common/security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import app.openschool.common.security.filters.JwtAuthenticationEntryPoint;
 import app.openschool.common.security.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +17,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@Profile("!openapi")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   private static final String[] PUBLIC_URLS = {
