6ca0af94d282cc480fde366825f70c69b57dbdcf
Tigran_Martirosyan1 <Tigran_Martirosyan1@email.com>
2022-05-18 20:55:12 +0400

    OS-91: integrate translation logic by i18next

--- os-ui/src/component/AppRoutes/Routes.tsx
+++ os-ui/src/component/AppRoutes/Routes.tsx
@@ -1,29 +1,17 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
 import { RootState } from '../../redux/Store';
 import ProtectedRoute from './ProtectedRoute';
 import Homepage from '../../pages/Homepage/Homepage';
 import ChooseCategoryPage from '../../pages/ChooseCategoryPage/ChooseCategoryPage';
 import MyLearningPathPage from '../../pages/MyLearningPathPage/MyLearningPathPage';
-import NotFound from '../../pages/NotFound/NotFound';
 
 const AppRoutes = () => {
-  const [isVerify, setIsVerify] = useState(true);
-  const verifyInfo = {
-    status: true,
-    error: false,
-    data: {
-      id: 'jgr874gefrt8',
-    },
-  };
-
   const userInfo = useSelector<RootState>((state) => state.userInfo);
   return (
     <Routes>
       <Route path="/homepage" element={<Homepage />} />
       <Route path="/" element={<Navigate replace to="/homepage" />} />
-      {isVerify && <Route path="/verify/1" element={isVerify ? <ChooseCategoryPage /> : <NotFound />} />}
       <Route
         path="/categories/subcategories/userId=:id"
         element={(
