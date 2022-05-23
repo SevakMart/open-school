794da9df1bc65fdcf51f8f964008a94b9968fd4e
Tigran_Martirosyan1 <Tigran_Martirosyan1@email.com>
2022-05-23 19:09:03 +0400

    OS-91: rollback setupProxy and String.js

--- os-ui/src/setupProxy.js
+++ os-ui/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = createProxyMiddleware({
-  target: 'http://localhost:8080',
+  target: 'http://localhost:5000',
 });
 
 module.exports = function (app) {
