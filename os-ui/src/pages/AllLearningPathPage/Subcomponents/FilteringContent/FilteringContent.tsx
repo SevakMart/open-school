19c669e9cd9bb4624601dc52c879af0d0e058005
Avedis Demirdjian <avedis_demirdjian@epam.com>
2022-06-05 14:08:59 +0400

    OS-53:Add test file for FilteringContent component

--- os-ui/src/pages/AllLearningPathPage/Subcomponents/FilteringContent/FilteringContent.tsx
+++ os-ui/src/pages/AllLearningPathPage/Subcomponents/FilteringContent/FilteringContent.tsx
@@ -10,13 +10,19 @@ const FilteringContent = ({ title, content, filterFeature }:
   } = styles;
   return (
     <div className={filteringContent}>
-      <p className={mainFilteringContentTitle}>{title}</p>
+      <p data-testid={title} className={mainFilteringContentTitle}>{title}</p>
       {
           content && Object.entries(content).map((innerContent, index) => {
             if (typeof innerContent[1] === 'object') {
               return (
                 <div className={filteringSubContent} key={index}>
-                  <p className={filteringSubContentTitle}>{innerContent[0]}</p>
+                  <p
+                    data-testid={innerContent[0]}
+                    className={filteringSubContentTitle}
+                  >
+                    {innerContent[0]}
+
+                  </p>
                   {
                     Object.entries(innerContent[1]).length
                     && Object.entries(innerContent[1]).map((subcontent, i) => (
