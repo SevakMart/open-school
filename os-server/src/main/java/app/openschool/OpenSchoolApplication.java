b9f123e428c746f80a0d5be5ac8acaa4417e49ff
Sargis Sargsyan <sargiss987@gmail.com>
2022-07-20 10:53:15 +0400

    OS-128:Fixed_browser_reload_bug

--- os-server/src/main/java/app/openschool/OpenSchoolApplication.java
+++ os-server/src/main/java/app/openschool/OpenSchoolApplication.java
@@ -2,17 +2,15 @@ package app.openschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 @EnableAsync
 public class OpenSchoolApplication {
 
   public static void main(String[] args) {
-
     SpringApplication.run(OpenSchoolApplication.class, args);
   }
 
