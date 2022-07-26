a018bffe8af804e88bfb78f66b863ec088dd37cc
Sargis Sargsyan <sargiss987@gmail.com>
2022-07-26 11:19:08 +0400

    OS-147:Resolve_merge_conflict

--- os-ui/src/component/AppRoutes/Routes.tsx
+++ os-ui/src/component/AppRoutes/Routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Suspense } from 'react';
 import { RootState } from '../../redux/Store';
 import ProtectedRoute from './ProtectedRoute';
 import Homepage from '../../pages/Homepage/Homepage';
@@ -9,48 +10,51 @@ import AllLearningPathPage from '../../pages/AllLearningPathPage/AllLearningPath
 import AllMentorsPage from '../../pages/AllMentorsPage/AllMentorsPage';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 import AfterVerificationPage from '../../pages/AfterVerificationPage/AfterVerificationPage';
+import Loader from '../Loader/Loader';
 
 const AppRoutes = () => {
   const userInfo = useSelector<RootState>((state) => state.userInfo);
   return (
-    <Routes>
-      <Route path="/homepage" element={<Homepage />} />
-      <Route path="/homepage/account" element={<AfterVerificationPage />} />
-      <Route path="/" element={<Navigate replace to="/homepage" />} />
-      <Route
-        path="/categories/subcategories"
-        element={(
-          <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
-            <ChooseCategoryPage />
-          </ProtectedRoute>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/homepage/account" element={<AfterVerificationPage />} />
+        <Route path="/" element={<Navigate replace to="/homepage" />} />
+        <Route
+          path="/categories/subcategories"
+          element={(
+            <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
+              <ChooseCategoryPage />
+            </ProtectedRoute>
      )}
-      />
-      <Route
-        path="/myLearningPath"
-        element={(
-          <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
-            <MyLearningPathPage />
-          </ProtectedRoute>
+        />
+        <Route
+          path="/myLearningPath"
+          element={(
+            <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
+              <MyLearningPathPage />
+            </ProtectedRoute>
    )}
-      />
-      <Route
-        path="/exploreLearningPaths"
-        element={(
-          <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
-            <AllLearningPathPage />
-          </ProtectedRoute>
+        />
+        <Route
+          path="/exploreLearningPaths"
+          element={(
+            <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
+              <AllLearningPathPage />
+            </ProtectedRoute>
         )}
-      />
-      <Route
-        path="/mentors"
-        element={(
-          <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
-            <AllMentorsPage />
-          </ProtectedRoute>
+        />
+        <Route
+          path="/mentors"
+          element={(
+            <ProtectedRoute token={(userInfo as any).token ? (userInfo as any).token : null}>
+              <AllMentorsPage />
+            </ProtectedRoute>
         )}
-      />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 };
 export default AppRoutes;
