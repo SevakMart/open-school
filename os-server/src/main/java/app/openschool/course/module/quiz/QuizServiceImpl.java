2d029a15d0e159b8ff71f6e68d5a1560fec482b5
Sargis Sargsyan <sargiss987@gmail.com>
2022-09-02 11:59:36 +0400

    OS-150:Validate current mentor

--- os-server/src/main/java/app/openschool/course/module/quiz/QuizServiceImpl.java
+++ os-server/src/main/java/app/openschool/course/module/quiz/QuizServiceImpl.java
@@ -1,9 +1,11 @@
 package app.openschool.course.module.quiz;
 
+import app.openschool.course.module.Module;
 import app.openschool.course.module.ModuleRepository;
 import app.openschool.course.module.quiz.api.dto.CreateQuizDto;
 import app.openschool.course.module.quiz.api.mapper.QuizMapper;
 import java.util.Optional;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,8 +25,16 @@ public class QuizServiceImpl implements QuizService {
         .findById(moduleId)
         .map(
             module -> {
+              checkIfTheModuleBelongsToCurrentMentor(module);
               Quiz quiz = QuizMapper.createQuizDtoToQuiz(createQuizDto, module);
               return quizRepository.save(quiz);
             });
+  }
+
+  private void checkIfTheModuleBelongsToCurrentMentor(Module module) {
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    if (!module.getCourse().getMentor().getName().equals(username)) {
+      throw new IllegalArgumentException();
+    }
   }
 }
