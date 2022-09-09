4923e2250f19d797aaf4f332dcc28c4c0bb5b70c
Avedis Demirdjian <avedis_demirdjian@epam.com>
2022-09-09 23:28:44 +0400

    OS-154: Create my learning path main content

--- os-ui/src/pages/MyLearningPathPage/MyLearningPathPage.tsx
+++ os-ui/src/pages/MyLearningPathPage/MyLearningPathPage.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/Store';
-import NoCourses from './Subcomponents/NoCourses/NoCourses';
+import MyLearningPathMainContent from './Subcomponents/MainContent/MainContent';
 import Header from './Subcomponents/Header/Header';
-import InProgressCourse from './Subcomponents/InProgressCourse/InProgressCourse';
-import CompletedCourse from './Subcomponents/CompletedCourse/CompledtedCourse';
 import SuggestedCourses from './Subcomponents/SuggestedCourses/SuggestedCourses';
 import { EXPLORE_COURSES } from '../../constants/Strings';
-import { UserCourseType } from '../../types/UserCourseType';
 import styles from './MyLearningPathPage.module.scss';
 
 /* eslint-disable max-len */
@@ -16,10 +10,7 @@ import styles from './MyLearningPathPage.module.scss';
 const MyLearningPathPage = ({ userInfo }:any) => {
   const navigate = useNavigate();
   const { id: userId, token } = userInfo;
-  const [userCourses, setUserCourses] = useState<UserCourseType[]>([]);
-  const userEnrolledCourseByCourseStatus = useSelector<RootState>((state) => state.userEnrolledCourseByCourseStatus);
-  const { entity, isLoading, errorMessage } = userEnrolledCourseByCourseStatus as {entity:any[], isLoading:boolean, errorMessage:string};
-  const { courseContainer, exploreCourseButton } = styles;
+  const { exploreCourseButton } = styles;
 
   const exploreLearningPaths = () => {
     navigate('/exploreLearningPaths');
@@ -28,34 +19,8 @@ const MyLearningPathPage = ({ userInfo }:any) => {
   return (
     <>
       <Header userId={userId} token={token} />
-      <div className={courseContainer}>
-        {
-          userCourses.length === 0 ? <NoCourses />
-            : userCourses.map((userCourse, index) => {
-              if (userCourse.courseStatus === 'IN_PROGRESS') {
-                return (
-                  <InProgressCourse
-                    key={index}
-                    title={userCourse.title}
-                    courseStatus={userCourse.courseStatus}
-                    percentage={userCourse.percentage}
-                    remainingTime={userCourse.remainingTime}
-                    dueDate={userCourse.dueDate}
-                  />
-                );
-              }
-              return (
-                <CompletedCourse
-                  key={index}
-                  title={userCourse.title}
-                  courseStatus={userCourse.courseStatus}
-                  grade={userCourse.grade}
-                />
-              );
-            })
-        }
-        <button type="button" className={exploreCourseButton} onClick={exploreLearningPaths}>{EXPLORE_COURSES}</button>
-      </div>
+      <MyLearningPathMainContent />
+      <button type="button" className={exploreCourseButton} onClick={exploreLearningPaths}>{EXPLORE_COURSES}</button>
       <SuggestedCourses userId={userId} token={token} />
     </>
   );
---
+++ os-ui/src/pages/MyLearningPathPage/Subcomponents/MainContent/MainContent.tsx
@@ -0,0 +1,50 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../redux/Store';
+import Loader from '../../../../component/Loader/Loader';
+import NoCourses from '../NoCourses/NoCourses';
+import InProgressCourse from '../InProgressCourse/InProgressCourse';
+import CompletedCourse from '../CompletedCourse/CompledtedCourse';
+import { ErrorField } from '../../../../component/ErrorField/ErrorField';
+import styles from './MainContent.module.scss';
+
+/* eslint-disable max-len */
+
+const MyLearningPathMainContent = () => {
+  const userEnrolledCourseByCourseStatus = useSelector<RootState>((state) => state.userEnrolledCourseByCourseStatus);
+  const { entity, isLoading, errorMessage } = userEnrolledCourseByCourseStatus as {entity:any[], isLoading:boolean, errorMessage:string};
+  const { courseContainer } = styles;
+  return (
+    <div className={courseContainer}>
+      {isLoading && <Loader />}
+      {errorMessage !== '' && (
+      <ErrorField.MainErrorField className={['myLearningPathsErrorStyle']}>
+        {errorMessage}
+      </ErrorField.MainErrorField>
+      )}
+      {entity.length === 0 && !isLoading && <NoCourses /> }
+      {entity.length > 0 && entity.map((userCourse, index) => {
+        if (userCourse.courseStatus === 'IN_PROGRESS') {
+          return (
+            <InProgressCourse
+              key={index}
+              title={userCourse.title}
+              courseStatus={userCourse.courseStatus}
+              percentage={userCourse.percentage}
+              remainingTime={userCourse.remainingTime}
+              dueDate={userCourse.dueDate}
+            />
+          );
+        }
+        return (
+          <CompletedCourse
+            key={index}
+            title={userCourse.title}
+            courseStatus={userCourse.courseStatus}
+            grade={userCourse.grade}
+          />
+        );
+      }) }
+    </div>
+  );
+};
+export default MyLearningPathMainContent;
