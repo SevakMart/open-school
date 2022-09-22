43959c2d107093e5dd6fb587dc0a64037a84c9f8
Sargis Sargsyan <sargiss987@gmail.com>
2022-09-22 14:10:43 +0400

    OS-150:revert DifficultyRepository format change

--- os-server/src/main/java/app/openschool/course/difficulty/DifficultyRepository.java
+++ os-server/src/main/java/app/openschool/course/difficulty/DifficultyRepository.java
@@ -1,5 +1,7 @@
 package app.openschool.course.difficulty;
 
+import app.openschool.course.difficulty.Difficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DifficultyRepository extends JpaRepository<Difficulty, Integer> {}
+public interface DifficultyRepository extends JpaRepository<Difficulty, Integer> {
+}
