72ede026053f56fcb8304f0e67776f7c469e785c
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-02-01 19:50:40 +0400

    OS-63:Course overview page

--- os-ui/src/App.tsx
+++ os-ui/src/App.tsx
@@ -1,12 +1,9 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './component/AppRoutes/Routes';
-// import CourseModulePage from './pages/CourseModulePage/CourseModulePage';
 
 const App = () => (
   <Router>
     <AppRoutes />
-
-    {/* <CourseModulePage /> */}
   </Router>
 );
 
--- os-ui/src/types/Dropdowntypes.ts
+++ os-ui/src/types/Dropdowntypes.ts
@@ -1,5 +1,3 @@
-import { ReactElement } from 'react';
-
 export interface DropdownTypes {
     trigger: JSX.Element;
     menu: any;
