fb906329272b34d6154db50ecec427f9edd1d135
Hakob777 <85955002+HakobTono@users.noreply.github.com>
2023-02-10 18:58:52 +0400

    Update EnrolledCourseRepository.java

--- os-server/src/main/java/app/openschool/course/EnrolledCourseRepository.java
+++ os-server/src/main/java/app/openschool/course/EnrolledCourseRepository.java
@@ -1,8 +1,8 @@
 package app.openschool.course;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface EnrolledCourseRepository extends JpaRepository<EnrolledCourse, Long> {
 
