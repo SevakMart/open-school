aa2ba9dba469fee934896293cb3bfdb02fa462f5
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-03-19 22:59:39 +0400

    OS-317:Pop-up closes after clicking around the pop-up

--- os-ui/src/component/Portal/Components/FormPortal/FormPortal.tsx
+++ os-ui/src/component/Portal/Components/FormPortal/FormPortal.tsx
@@ -1,4 +1,7 @@
 import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { closeModal } from '../../../../redux/Slices/PortalOpenStatus';
 import styles from './FormPortal.module.scss';
 
 const portalElement = document.createElement('div');
@@ -8,10 +11,16 @@ document.body.appendChild(portalElement);
 export const FormPortal = ({ isOpen, children }:{isOpen:boolean, children:any}) => {
   if (!isOpen) return null;
   const { overlayStyles, modalStyles } = styles;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleOverlayClick = () => {
+    dispatch(closeModal());
+    navigate('/homepage');
+  };
 
   return createPortal(
     <>
-      <div className={overlayStyles} />
+      <div className={overlayStyles} onClick={handleOverlayClick} />
       <div className={modalStyles}>
         {children}
       </div>
--- os-ui/src/component/Portal/Components/FormPortal/__test__/FormPortal.test.tsx
+++ os-ui/src/component/Portal/Components/FormPortal/__test__/FormPortal.test.tsx
@@ -1,9 +1,22 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../../../../redux/Store';
 import { FormPortal } from '../FormPortal';
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom') as any),
+  useNavigate: () => mockedUseNavigate,
+}));
 
 describe('Create unit tests for FormPortal component', () => {
   test('Create a snapshot test', () => {
-    const { asFragment } = render(<FormPortal isOpen children={<h2>Hello world</h2>} />);
+    const { asFragment } = render(
+      <Provider store={store}>
+        <FormPortal isOpen children={<h2>Hello world</h2>} />
+      </Provider>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
