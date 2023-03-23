12864378453bd8e746d9625a8624cb94afe34335
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-03-23 23:32:24 +0400

    bug/OS-215: Searched mentors aren't saved on the Mentors page when the user switches to other page

--- os-ui/src/component/Input/Components/SearchInput.tsx
+++ os-ui/src/component/Input/Components/SearchInput.tsx
@@ -1,35 +1,64 @@
-import React, { useRef } from 'react';
+import React, {
+  useRef, useEffect, useMemo, useState, createContext,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from '../Input-Styles.module.scss';
 import SearchIcon from '../../../icons/Search';
 import { useFocus } from '../../../custom-hooks/useFocus';
 import { SearchProps } from '../../../types/SearchType';
 
-export const SearchInput = ({ changeUrlQueries, className }:SearchProps) => {
+interface SearchContextType {
+  searchQuery: string;
+  setSearchQuery?: (query: string) => void;
+}
+
+const SearchContext = createContext<SearchContextType>({
+  searchQuery: '',
+  setSearchQuery: undefined,
+});
+
+export const SearchInput = ({ changeUrlQueries, className }: SearchProps) => {
   const { t } = useTranslation();
-  const styleNames = className.map((className:string) => styles[`${className}`]);
-  const inputRef = useRef<HTMLInputElement|null>(null);
-  const searchContainerRef = useRef<HTMLDivElement|null>(null);
+  const styleNames = className.map((className: string) => styles[className]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const searchContainerRef = useRef<HTMLDivElement | null>(null);
   const [handleOnFocus, handleOnBlur] = useFocus('white', searchContainerRef.current);
-  const handleSearch = (e:React.KeyboardEvent) => {
-    if (e.key === 'Enter' && inputRef.current && inputRef.current.value) {
-      return changeUrlQueries(inputRef.current.value);
-    } if (e.key === 'Enter' && inputRef.current && inputRef.current.value === '') {
-      return changeUrlQueries('');
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = () => {
+    if (inputRef.current && inputRef.current.value) {
+      setSearchQuery(inputRef.current.value);
+      changeUrlQueries(inputRef.current.value);
+    } else {
+      setSearchQuery('');
+      changeUrlQueries('');
     }
   };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = searchQuery;
+      changeUrlQueries(searchQuery);
+    }
+  }, [searchQuery, changeUrlQueries]);
+
+  const memoizedValue = useMemo(() => ({ searchQuery, setSearchQuery: (query: string) => setSearchQuery(query) }), [searchQuery, setSearchQuery]);
+
   return (
-    <div className={styleNames.join(' ')} ref={searchContainerRef}>
-      <SearchIcon />
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder={t('string.search')}
-        data-testid="searchInput"
-        onKeyPress={handleSearch}
-        onFocus={() => handleOnFocus()}
-        onBlur={() => handleOnBlur()}
-      />
-    </div>
+    <SearchContext.Provider value={memoizedValue}>
+      <div className={styleNames.join(' ')} ref={searchContainerRef}>
+        <SearchIcon />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder={t('string.search')}
+          data-testid="searchInput"
+          onChange={handleSearch}
+          onFocus={() => handleOnFocus()}
+          onBlur={() => handleOnBlur()}
+        />
+      </div>
+    </SearchContext.Provider>
   );
 };
