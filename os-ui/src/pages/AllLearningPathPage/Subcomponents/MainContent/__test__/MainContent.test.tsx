3564a8fbc22d88613dd201fa9e16f3ce90a37baf
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-03-28 19:35:24 +0400

    OS-266: Updated test cases

--- os-ui/src/pages/AllLearningPathPage/Subcomponents/MainContent/__test__/MainContent.test.tsx
+++ os-ui/src/pages/AllLearningPathPage/Subcomponents/MainContent/__test__/MainContent.test.tsx
@@ -1,7 +1,10 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from '../../../../../redux/Store';
+import i18next from 'i18next';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
+import { Suspense } from 'react';
 import MainContent from '../MainContent';
+import { store } from '../../../../../redux/Store';
 
 const mockUseNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -12,11 +15,23 @@ jest.mock('react-router-dom', () => ({
   }),
 }));
 
+i18next.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 describe('Create test cases for CourseContentHeader', () => {
   test('Make a snapshot test', () => {
     const { asFragment } = render(
       <Provider store={store}>
-        <MainContent />
+        <I18nextProvider i18n={i18next}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <MainContent />
+          </Suspense>
+        </I18nextProvider>
       </Provider>,
     );
     expect(asFragment()).toMatchSnapshot();
