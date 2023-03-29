34a997af43dcd482a11341941474a96e6c35a69b
gayane_chilingaryan <gayane_chilingaryan@epam.com>
2023-03-29 14:28:37 +0400

    OS-325: Updated unit test

--- os-ui/src/pages/AllLearningPathPage/Subcomponents/MainContent/__test__/MainContent.test.tsx
+++ os-ui/src/pages/AllLearningPathPage/Subcomponents/MainContent/__test__/MainContent.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import { store } from '../../../../../redux/Store';
+import { store as myStore } from '../../../../../redux/Store';
 import { reactRouterDomMock } from '../../../../AllMentorsPage/Subcomponents/MainContent/__test__/react-router-dom-mock';
 import MainContent from '../MainContent';
 
@@ -10,7 +10,7 @@ jest.doMock('react-router-dom', () => reactRouterDomMock);
 describe('Create test cases for CourseContentHeader', () => {
   test('Make a snapshot test', () => {
     const { asFragment } = render(
-      <Provider store={store}>
+      <Provider store={myStore}>
         <MemoryRouter>
           <MainContent />
         </MemoryRouter>
