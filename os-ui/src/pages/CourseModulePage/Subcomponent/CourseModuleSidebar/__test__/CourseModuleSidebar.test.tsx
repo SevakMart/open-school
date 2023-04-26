09ee055dce16c0167f01a8b8ea42e41e2042810b
Ed <ed960@mail.ru>
2023-04-26 11:07:30 +0400

    x

--- os-ui/src/pages/CourseModulePage/Subcomponent/CourseModuleSidebar/__test__/CourseModuleSidebar.test.tsx
+++ os-ui/src/pages/CourseModulePage/Subcomponent/CourseModuleSidebar/__test__/CourseModuleSidebar.test.tsx
@@ -6,15 +6,6 @@ import rootReducer from '../../../../../redux/RootReducer';
 import CourseModuleSidebar from '../CourseModuleSidebar';
 
 describe('CourseModuleSidebar', () => {
-  // const handleChangeValueMock = jest.fn();
-  // const modulesMock = [{ title: 'Module 1', description: 'Module 1 description', moduleItemSet: [{}, {}] },
-  //   {
-  //     title: 'Module 2',
-  //     description: 'Module 2 description',
-  //     moduleItemSet: [{}, {}],
-  //   },
-  // ];
-
   const store = createStore(rootReducer);
 
   test('renders the component', () => {
@@ -25,10 +16,7 @@ describe('CourseModuleSidebar', () => {
         </BrowserRouter>
       </Provider>,
     );
-    // expect(screen.getByText('Course Name')).toBeInTheDocument();
     expect(screen.getByText('Overview')).toBeInTheDocument();
-    // expect(screen.getByAltText('chevron')).toBeInTheDocument();
-    // expect(screen.getByTestId('moduleListOpen')).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 
@@ -41,11 +29,9 @@ describe('CourseModuleSidebar', () => {
       </Provider>,
     );
     fireEvent.click(screen.getByAltText('chevron'));
-    // expect(screen.getByTestId('moduleListClosed')).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
 
     fireEvent.click(screen.getByAltText('chevron'));
-    // expect(screen.getByTestId('moduleListOpen')).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 
@@ -58,7 +44,6 @@ describe('CourseModuleSidebar', () => {
       </Provider>,
     );
     fireEvent.click(screen.getByText('Overview'));
-    // expect(handleChangeValueMock).toHaveBeenCalledWith('Overview');
     expect(asFragment()).toMatchSnapshot();
   });
 });
